45b0e1a0ac082545e378bdaa846de372dd1b006c
Data Comm <dc@milliways.bcit.ca>
2020-01-23 12:03:01 -0800

    working

--- multi.c
+++ multi.c
@@ -9,16 +9,12 @@
 #define _REENTRANT
 #define DCE_COMPAT
 
-pthread_mutex_t count_mutex     = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t condition_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t  condition_cond  = PTHREAD_COND_INITIALIZER;
-
 /*----------- Function Prototypes ------------------*/
 void* producer(void*);
 void* consumer(void*);
 
 typedef struct {
-	char c;
+	char *c;
 	int i;
 	double d;
 	int update;
@@ -27,29 +23,19 @@ typedef struct {
 /*------------ Mutex Variables ---------------------*/
 pthread_mutex_t threadLock = PTHREAD_MUTEX_INITIALIZER;
 
+pthread_cond_t condition_mutex = PTHREAD_COND_INITIALIZER;
 
 int main (void)
 {
-  // pthread_t thread1, thread2;
-  // pthread_attr_t attr1, attr2;
+  pthread_t thread1, thread2;
 
   ThreadInfo info_ptr;
-  info_ptr.c = 'a';
+  info_ptr.c = "Hi";
   info_ptr.i = 1;
   info_ptr.d = 10.5;
   info_ptr.update = 0;
-  info_ptr.done = 0;
-
-  // int high = 500, low = 100;
-  // struct sched_param par;
-  // struct sched_param par2;
-  // pthread_attr_init(&attr1);
-  // pthread_attr_init(&attr2);
-  // pthread_setschedpolicy(&attr1, SCHED_RR);
-  // pthread_setschedpolicy(&attr2, SCHED_RR);
-  // Create and run the threads.
-  pthread_create (&thread1, &attr1, producer, (ThreadInfo*)&info_ptr);
-  pthread_create (&thread2, &attr2, consumer, (ThreadInfo*)&info_ptr);
+  pthread_create (&thread1, 0, producer, (ThreadInfo*)&info_ptr);
+  pthread_create (&thread2, 0, consumer, (ThreadInfo*)&info_ptr);
 
   pthread_join (thread1, NULL);
   pthread_join (thread2, NULL);
@@ -62,26 +48,22 @@ int main (void)
 void* producer(void* arg){
   ThreadInfo* info = (ThreadInfo*)arg;
 
-	char a = 'a';
-	int i = 3;
-	double d = 10.5;
-	for (int i = 0;i < 3; i ++)
+	int i = 0;
+  char *msg[] ={"Hello","Bonjour","Ni Hao"};
+	for (;i < 3;i++)
 	{
+	 		pthread_mutex_lock(&threadLock);
+      printf("Updating\r\n");
+      info->c = msg[i];
+      info->i = i;
+      info->d++;
+	    pthread_cond_wait(&condition_mutex,&threadLock);
+	 		pthread_mutex_unlock(&threadLock);
 
-    printf("Mut %d",info->update);
-		if (info->update == 0) {
-      printf("Mut");
-			pthread_mutex_lock(&threadLock);
-			info->c = a++;
-			info->i = i++;
-			info->d = d++;
-			info->update = 1;
-			if (i == 6)
-				info->done = 1;
+      pthread_mutex_lock(&threadLock);
+      pthread_cond_signal(&condition_mutex);
 			pthread_mutex_unlock(&threadLock);
-		}
-		if (info->done)
-			break;
+
 	}
 	return NULL;
 }
@@ -89,17 +71,20 @@ void* consumer(void* arg){
   
   ThreadInfo* info = (ThreadInfo*)arg;
 
-	for (int i = 0;i < 3; i ++)
+	int i = 0;
+	for (;i < 3;i++)
 	{
-		if (info->update == 1) {
 			pthread_mutex_lock(&threadLock);
-      printf("Consumer");
-      printf("Char: %s Int %d Double %d",info->c ,info->i, info->d);
-			info->update = 0;
+      pthread_cond_signal(&condition_mutex);
 			pthread_mutex_unlock(&threadLock);
-		}
-		if (info->done)
-			break;
+
+      pthread_mutex_lock(&threadLock);
+      printf("String:%s Int:%d Double:%f\r\n", info->c,info->i,info->d);
+	    pthread_cond_wait(&condition_mutex,&threadLock);
+	 		pthread_mutex_unlock(&threadLock);
+
+
+		
 
 	}
 	return NULL;
